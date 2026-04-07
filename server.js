bcfae953c51bf43f9cd67d2f5b11e6e1855403b3
BornaVuksanovic <boco.vuksanovic2@gmail.com>
2026-04-07 09:14:19 +0200

    aa

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+console.log("Test porta:", process.env.PORT);
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('Pozdrav sa Railway servera!');
 });
